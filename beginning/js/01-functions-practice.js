0acdbf3c343c1d0ebca1a4faf278ceae2fce31d0
kaforney <kaforney6686@gmail.com>
2018-03-01 14:42:25 -0800

    Part 1

---
+++ beginning/js/01-functions-practice.js
@@ -0,0 +1,46 @@
+/* eslint-env browser */
+
+/*
+//STEP 1
+function halfNumber(wholeNumber) {
+    window.console.log("Half of " + wholeNumber + " is " + (wholeNumber / 2) + ".");  
+}
+halfNumber(12);
+
+//STEP 2
+
+function squareNumber(oneNumber) {
+    window.console.log("The result of squaring the number " + oneNumber + " is " + Math.pow(oneNumber, oneNumber) + ".");
+}
+squareNumber(4);
+
+//STEP 3
+function percentOf(firstNumber, secondNumber) {
+    var perCent = Number(firstNumber / secondNumber) * 100;
+    window.console.log(firstNumber + " is " + perCent + "% of " + secondNumber);
+}
+percentOf(3, 4);
+
+//STEP 4
+function findModulus(numberOne, numberTwo) {
+    var calculateModulus = numberOne % numberTwo;
+    window.console.log(calculateModulus + " is the modulus of " + numberOne + " and " + numberTwo + ".");
+}
+findModulus(12, 5);
+*/
+//STEP 5
+function calculate(numbers) {
+    window.prompt("Input a number.");
+    window.prompt("Input another number.");
+    window.prompt("Input another number.");
+    window.prompt("Input another number.");
+    document.write("You ")
+/*
+    var base = document.prompt
+    for (var i = 1; i < arguments.length; i++) {
+        base += Number(arguments[i]);
+    }
+    window.console.log(base);
+}
+calculate()
+*/
